0b159269dca355781f126daa592ad52b767cc18f
BRBX <79046011+BRBXGIT@users.noreply.github.com>
2025-03-08 23:26:20 +0300

    Update AuthUtils.kt

--- feature/auth/src/main/java/com/example/auth/utils/AuthUtils.kt
+++ feature/auth/src/main/java/com/example/auth/utils/AuthUtils.kt
@@ -1,5 +1,5 @@
 package com.example.auth.utils
 
 object AuthUtils {
-    const val CLIENT_ID = 22328
+    const val CLIENT_ID = 12345 //Not a real client id, cause i don't know can i push it to git or no :) 
 }
